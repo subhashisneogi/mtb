c80f8b0a43de5e524da2369106bea0ee3c0890b9
subhashisneogi <138887519+subhashisneogi@users.noreply.github.com>
2026-04-08 04:25:56 +0530

    update

--- views.py
+++ views.py
@@ -1,21 +1,22 @@
 @permission_required_admin
 def users_profile_list(request):
-    factory = APIRequestFactory()
+	factory = APIRequestFactory()
+	drf_request = factory.get('/users-profile-list/', data=request.GET)
+	drf_request.user = request.user
+	# pass auth header if exists
+	if request.META.get('HTTP_AUTHORIZATION'):
+		drf_request.META['HTTP_AUTHORIZATION'] = request.META['HTTP_AUTHORIZATION']
+	response = UserProfileAPIView.as_view()(drf_request)
 
-    drf_request = factory.get('/users-profile-list/', data=request.GET)
-    drf_request.user = request.user
+	data = response.data
+	print("data", data)
+	context = {
+		"data": data.get("results", data),
+		"count": data.get("count"),
+		"next": data.get("next"),
+		"previous": data.get("previous"),
+	}
 
-    # pass auth header if exists
-    if request.META.get('HTTP_AUTHORIZATION'):
-        drf_request.META['HTTP_AUTHORIZATION'] = request.META['HTTP_AUTHORIZATION']
+	return render(request, 'profile/user_profile_lists.html', context)
 
-    response = UserProfileAPIView.as_view()(drf_request)
-
-    data = response.data
-
-    return render(request, 'profile/user_list.html', {
-        "data": data.get("results", data),
-        "count": data.get("count"),
-        "next": data.get("next"),
-        "previous": data.get("previous"),
-    })
+data {'detail': ErrorDetail(string='Authentication credentials were not provided.', code='not_authenticated')}
